7079138db5be063b362e3b88dba230e075d58fde
codingverse24 <codingverse2024@gmail.com>
2024-12-09 09:21:43 +0530

    updated meta tags

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,16 @@ import "antd/dist/antd.css";
 
 import Router from "./router";
 import i18n from "./translation";
+import mixpanel from "mixpanel-browser";
 
+ 
+// Near entry of your product, init Mixpanel
+
+mixpanel.init("b49a53e30f92a2454eef35b1961a4f87", {
+  debug: true,
+  track_pageview: true,
+  persistence: "localStorage",
+});
 const App = () => (
   <BrowserRouter>
     <I18nextProvider i18n={i18n}>
